2f6659c4388ce12c7c1994c09b005861bc611901
alonsopayaweb <56549494+alonsopayaweb@users.noreply.github.com>
2021-06-18 14:45:59 -0400

    Clave de setting.py protegida

--- OngPeludo/settings.py
+++ OngPeludo/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g0*$64&lq!nkeapue^gm)9$+9j0*^5u!$^u$vhm+q4lyb+q)yb'
+SECRET_KEY = 'Dile al Alonso que te dé la clave xd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
